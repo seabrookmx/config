2e0ab7b429261887042831cbcde09dd3468087ab
Tyler Seabrook <seabrook.mx@gmail.com>
2020-09-22 15:13:00 -0700

    Update hvinstall.sh

--- hvinstall.sh
+++ hvinstall.sh
@@ -22,6 +22,7 @@ git \
 vim \
 zip \
 unzip \
+httpie \
 dotnet-sdk-2.2 \
 docker-ce;
 
@@ -31,19 +32,32 @@ curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.s
 sudo apt-get install git-lfs && \
 git lfs install;
 
-echo '**   Set up docker-compose and user.   **'
-sudo pip install docker-compose
+echo '**   Set up virtualenv, docker-compose, and docker user.   **'
+sudo pip install virtualenv docker-compose
 sudo usermod -aG docker $USER
+
+echo '**   Installing kubectl   **'
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+echo '**   Installing kubectx/kubens   **'
+git clone https://github.com/ahmetb/kubectx.git ~/.kubectx
+COMPDIR=$(pkg-config --variable=completionsdir bash-completion)
+ln -sf ~/.kubectx/completion/kubens.bash $COMPDIR/kubens
+ln -sf ~/.kubectx/completion/kubectx.bash $COMPDIR/kubectx
+echo '#kubectx and kubens' >> ~/.profile
+echo 'export PATH=$PATH:$HOME/.kubectx' >> ~/.profile
 
 echo '**   Installing NVM (Node Version Manager) and NodeJS   **'
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.35.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-nvm install 10
+nvm install 14
 
-echo '**   Configure Go 1.12 (AMD64)   **'
-wget "https://dl.google.com/go/go1.12.14.linux-amd64.tar.gz"
-sudo tar -C /usr/local -xzf go1.12.14.linux-amd64.tar.gz
+echo '**   Configure Go 1.15.2 (AMD64)   **'
+wget "https://dl.google.com/go/go1.15.2.linux-amd64.tar.gz"
+sudo tar -C /usr/local -xzf go1.15.2.linux-amd64.tar.gz
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
 
 echo '**   Setting default terminal editor to vim   **'
